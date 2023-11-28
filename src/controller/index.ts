5de06e80c00cdd762848eab21da65432492f85a7
ifyour <ifyour@outlook.com>
2023-11-28 23:52:07 +0800

    feat: optimized router module group

---
+++ src/controller/index.ts
@@ -0,0 +1,4 @@
+import { auth } from './auth'
+import { post } from './post'
+
+export { auth, post }
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,19 @@ import { jwt } from 'hono/jwt'
 import { serve } from '@hono/node-server'
 
 import { SECRET_TOKEN } from './config/env'
-import { post } from './controller/post'
+import { post, auth } from './controller'
 
 const app = new Hono()
 
+/** middleware */
 app.use('/auth/*', jwt({ secret: SECRET_TOKEN, cookie: 'jwt-token' }))
 
+/** router */
 app.get('/', (c) => c.json({ hello: 'hono!' }))
-
 app.route('/post', post)
+app.route('/auth', auth)
 
+/** global error handling */
 app.onError((error: CustomError, c) => {
   if (error.status === 401) {
     return c.json({ code: 401, message: 'Unauthorized', data: null }, 401)
